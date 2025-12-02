f35676c7034d9841b479ee21ec7e1ff872f29633
Lady Cans <frc2881@gmail.com>
2025-12-01 20:47:50 -0600

    Update constants with robot to camera transform calculation utility ref

--- core/constants.py
+++ core/constants.py
@@ -213,6 +213,13 @@ class Sensors:
       fallbackPoseStrategy = PoseStrategy.LOWEST_AMBIGUITY
     )
 
+    # # DEBUG: use for empirical calculation of robot to camera transform for each pose sensor configuration using fixed target measured from robot and targetPose averages from PhotonVision
+    # robotToCamera = utils.getRobotToCameraTransform(
+    #   targetToRobot = Transform3d(Translation3d(0.860, 0, -0.311), Rotation3d().fromDegrees(0, 0, -180.0)),
+    #   targetToCamera = Transform3d(Translation3d(.707, .041, -0.089), Rotation3d(Quaternion(-0.01, -0.09, 0.0, -0.99)))
+    # )
+    # logger.debug(robotToCamera)
+
     kPoseSensorConfigs: tuple[PoseSensorConfig, ...] = (
       PoseSensorConfig(
         "FrontLeft",
